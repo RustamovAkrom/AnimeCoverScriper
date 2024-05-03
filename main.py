2d73df114d6c95bd863c41ebcfe788a4f2242172
Akromjon <akromjonrustamov56@gmail.com>
2024-05-03 22:39:16 +0500

    parsing images

--- main.py
+++ main.py
@@ -1,47 +1,52 @@
 from anime import anime_scriping
-from db import DBConnection
-import json
+from multiprocessing import Process
+import requests
+import os
 
 
-def appender_on_json_file(filename: str, data: dict):
-    with open(filename, "w") as json_file:
-        json.dump(data, json_file, indent=4)
+if __name__=="__main__":
 
-    
-def main():
-    
     response = anime_scriping(input("Search anime type --> "))
 
     if response != "Now data":
-
         for response in response:
-            id = int(response['id'])
-            type = str(response['type'])
-            created_at = str(response['created_at'])
-            updated_at = str(response['updated_at'])
-            description = str(response['description'])
-            cover = str(response['cover'])
-            youtube_video = str(response['youtube_video'])
-
-            data = {
-                "id":id,
-                "type":"type",
-                "created_at":created_at,
-                "updated_at":updated_at,
-                "description":description,
-                "cover":cover,
-                "video":youtube_video
-            }
-            print(data)
-            # appender_on_json_file("data.json", data)
-            # connection.add_image(image_id = id, image_type = type,
-            #                      created_at = created_at, updated_at = updated_at,
-            #                      description = description, cover = cover,
-            #                      youtube_video = youtube_video)
+            cover = response['cover']
             
+            if cover != "":
+                response_cover = requests.get(cover)
+                
+                dir = "data/"
+                if not os.path.exists(dir):
+                    os.mkdir(dir)
 
-if __name__=="__main__":
-    # connection = DBConnection()
-    # connection.create_all_tables()
+                file = str(response['type']) + "_" +response['id'] + ".jpg"
+
+                with open(dir + file, "wb") as file_wb:
+                    file_wb.write(response_cover.content)
+                    
+    # proccess = Process(target=main)
+    # proccess.start()
+
     
-    main()
+
+
+# if response != "Now data":
+        
+#         for response in response:
+#             id = int(response['id'])
+#             type = str(response['type'])
+#             created_at = str(response['created_at'])
+#             updated_at = str(response['updated_at'])
+#             description = str(response['description'])
+#             cover = str(response['cover'])
+#             youtube_video = str(response['youtube_video'])
+
+#             data = {
+#                 "id":id,
+#                 "type":type,
+#                 "created_at":created_at,
+#                 "updated_at":updated_at,
+#                 "description":description,
+#                 "cover":cover,
+#                 "video":youtube_video
+#             }
